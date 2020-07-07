05577368ccd7396ce67c01a73cc45dd28c7e698d
rami <rami.al-karo@holda.fi>
2020-07-07 14:18:36 +0300

    register profile added

--- client/src/views/register/index.js
+++ client/src/views/register/index.js
@@ -1,10 +1,115 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Container, CssBaseline, Avatar, TextField, Typography, Grid, FormControlLabel, Checkbox, Button, Link } from '@material-ui/core'
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
 
-const Register = ()=> {
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avater: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.primary.main
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(3)
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2)
+    }
+}))
+
+const Register = () => {
+    const classes = useStyles()
     return (
-        <div>
-            <h5>Register will be here...</h5>
-        </div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avater}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">Sign Up</Typography>
+
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="firstname"
+                                autoComplete="fname"
+                                id="firstName"
+                                label="First Name"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="lastname"
+                                autoComplete="lname"
+                                id="lastName"
+                                label="Last Name"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="email"
+                                autoComplete="email"
+                                id="email"
+                                label="Email address"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                type="password"
+                                name="password"
+                                autoComplete="current-password"
+                                id="password"
+                                label="Password"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                label="I want to receive inspiration, marketing promotions and updates via email."
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link href="/" variant="body2">
+                                Already have an account? Login
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
     )
 }
 
