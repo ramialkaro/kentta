b900cc48abf571bfc78d998c307cd9467c5370ed
rami <rami.al-karo@holda.fi>
2020-07-09 14:45:29 +0300

    connect client side to API using axios

--- API/server.js
+++ API/server.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
     res.json({ messsage: "welcome to Kentta application." })
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 require("./routes/player.routes")(app)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,19 @@ import Routes from './views/routes'
 import BottomBar from './components/BottomBar'
 import { ThemeProvider } from '@material-ui/core'
 import theme from './theme'
+import axios from './axios'
 
 function App() {
+
+  React.useEffect(()=>{
+    axios.get(`/player`)
+    .then(res=>{
+      console.log(res.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }, [])
   return (
     <ThemeProvider theme={theme}>
       <Routes />
