636225d587f7035ac23b1e7635b0d368212e30fd
ramialkaro <almemari22ramosh@gmail.com>
2020-07-10 09:00:17 +0300

    change the AWS url  for the config file and remove dev script from package file

--- API/config/db.config.js
+++ API/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports= {
-    HOST: "ec2-13-53-45-15.eu-north-1.compute.amazonaws.com",
+    HOST: "ec2-13-53-65-162.eu-north-1.compute.amazonaws.com",
     USER: "admin",
     PASSWORD:"passwd",
     DB:"kenttaDB"
