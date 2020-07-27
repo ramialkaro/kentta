27ff30b916f4cdf6a138339ba1293015511f9493
ramialkaro <almemari22ramosh@gmail.com>
2020-07-27 12:01:54 +0300

    remove unused proctectedRouter file

--- API/routes/protectedRouter.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const express = require('express')
-const {withJWTAuthMiddleware} = require('express-kun')
-const router = express.Router()
-
-const protectedRouter = withJWTAuthMiddleware(router, process.env.JWT_SECRET)
-
-module.exports = protectedRouter
