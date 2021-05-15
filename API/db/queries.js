216bdddfa3e95937327215359d0c01ac9b0d6b63
ramialkaro <rami.alkaro@gmail.com>
2021-05-15 22:51:52 +0300

    format the queries file

--- API/db/queries.js
+++ API/db/queries.js
@@ -1,7 +1,5 @@
-// export database queries
 
 const knex = require("./knex");
-const { update, first } = require("./knex");
 
 module.exports = {
   player: {
@@ -27,13 +25,7 @@ module.exports = {
 
   game: {
     getAll: function () {
-      return (
-        knex("games")
-          /* .join('team', function(){
-                this.on('game.id','=','team.game_id').onNotIn('player_id',player_id)
-            }) */
-          .orderBy("startTime")
-      );
+      return knex("games").orderBy("startTime");
     },
     getOne: function (id) {
       return knex("games").where({ id }).first();
@@ -42,7 +34,7 @@ module.exports = {
       return knex("games").insert(game);
     },
     update: function (id, game) {
-      return knex("games").where({ id }).update(gae);
+      return knex("games").where({ id }).update(game);
     },
     delete: function (id) {
       return knex("games").where({ id }).del();
@@ -51,25 +43,25 @@ module.exports = {
 
   team: {
     // [x] TODO first check if player has a game or "team" then
-    checkTeam: function ({ game_id, player_id }) {
-      return knex("teams").where({ game_id, player_id });
+    checkTeam: function ({ gameID, playerID }) {
+      return knex("teams").where({ gameID, playerID });
     },
 
     // [x] TODO get all games the i had been join them
 
-    myGames: function ({ player_id }) {
+    myGames: function ({ playerID }) {
       return knex("teams")
-        .where({ player_id })
-        .join("games", "team.game_id", "=", "game.id")
+        .where({ playerID })
+        .join("games", "team.gameID", "=", "game.id")
         .orderBy("startTime");
     },
 
     // [x] TODO case 1: Player already there, just passing game_id to get players OR team...
-    getAll: function ({ game_id }) {
+    getAll: function ({ gameID }) {
       return knex("teams")
-        .where({ game_id })
-        .join("games", "team.game_id", "=", "game.id")
-        .join("players", "team.player_id", "=", "player.id");
+        .where({ gameID })
+        .join("games", "team.gameID", "=", "game.id")
+        .join("players", "team.playerID", "=", "player.id");
     },
 
     // [x] TODO case 2: player not found in the table then inserting new one
@@ -78,8 +70,8 @@ module.exports = {
     },
 
     // [x] TODO case 3: Player want to leave from the game by deleting the ROW (player_id & game_id)
-    leave: function ({ game_id, player_id }) {
-      return knex("teams").where({ game_id, player_id }).del();
+    leave: function ({ gameID, playerID }) {
+      return knex("teams").where({ gameID, playerID }).del();
     },
   },
 };
